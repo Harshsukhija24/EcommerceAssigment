a78691946ed81d511463903e1fc31f4163e897a7
Harshsukhija24 <harshsukhija2002@gmail.com>
2024-06-28 17:54:22 +0530

    v2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./tailwind.css";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import ReduxProvider from "./Redux/Provider"; // Adjust the path as per your file structure
 
