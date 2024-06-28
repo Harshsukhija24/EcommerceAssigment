e68ed6bcff51be40e88ee932380e2bad44bc7199
Harshsukhija24 <harshsukhija2002@gmail.com>
2024-06-28 15:07:10 +0530

    v2

--- backend/Routes/UserRoute.js
+++ backend/Routes/UserRoute.js
@@ -1,16 +1,9 @@
 import express from "express";
-import {
-  registerUser,
-  loginUser,
-  logoutUser,
-  isTokenBlacklisted,
-} from "../controllers/userController.js";
-import auth from "../middleware/auth.js"; // Middleware to check for valid token
+import { registerUser, loginUser } from "../Controller/User.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/logout", auth, isTokenBlacklisted, logoutUser); // Protected logout route
 
 export default router;
